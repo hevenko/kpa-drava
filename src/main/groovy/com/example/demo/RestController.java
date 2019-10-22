91b0ecdf2ccfd879ce418a3834711d78e61a623f
hevenko <hevenko@gmail.com>
2019-10-22 19:57:42 +0200

    first commit

---
+++ src/main/groovy/com/example/demo/RestController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping(path = "/demo")
+public class RestController {
+
+    @GetMapping(path = "/dajIme")
+    public String getName() {
+        return "Ime";
+    }
+}
