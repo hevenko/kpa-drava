4a45438fdb58bb8f69e87c7a1a4b3befe29e0a9a
hevenko <hevenko@gmail.com>
2019-10-27 15:57:21 +0100

    probam bazi pristupiti

--- src/main/groovy/com/example/demo/DemoApplication.groovy
+++ src/main/groovy/com/example/demo/DemoApplication.groovy
@@ -1,13 +1,23 @@
 package com.example.demo
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class DemoApplication {
+class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private GlobalProperties globalProperties
 
 	static void main(String[] args) {
 		SpringApplication.run(DemoApplication, args)
 	}
 
+	@Override
+	void run(String... args) throws Exception {
+		System.out.println(globalProperties.password)
+		System.out.println("**********rajko**********: "+globalProperties.url)
+	}
 }
